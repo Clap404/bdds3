1719a70e88c5ffc2d3be3c3a03f94a39ede04803
Benoit H <cl.clap@laposte.net>
2013-10-25 13:19:51 +0200

    un peu de css, un nouvelle icone, tout ça…

--- view/accueil_v.php
+++ view/accueil_v.php
@@ -1,30 +1,16 @@
 <?php ob_start() ?>
 
 <div class="leftBlock">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<p>Merci d'avoir choisi cette application de gestion de manifestation sportive pour iut.</p>
+	<p>Si vous êtes dérouté par l'interface, vous pouvez consulter <a href="README.html">le readme</a></p>
 </div>
 
 <div class="midBlock">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<p>La base de données générée avec le fichier <code>jeu_de_test.sql</code> n'assigne pas nécéssairement d'étudiants à chaque épreuve ou manifestation.</p>
 </div>
 
 <div class="rightBlock">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<p>Malgré toute l'attention porté à ce produit, il se peut que vous trouviez quelques bugs. Si tel est le cas, nous tenterons de les corriger pour la prochaine version.</p>
 </div>
 
 <?php $content = ob_get_clean() ?>
